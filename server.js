65bca1c98d700e9ec32f59e06977703e0a85d2e2
jobair-talha <talhajobair10@gmail.com>
2022-10-15 10:01:01 +0600

    first practics graphql

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./scheme");
+
+// start apollo server
+const startApolloServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/graphql" });
+  console.log(
+    `Apollo Server Listening on http://localhost:${process.env.PORT}/graphql`
+  );
+};
+
+app.get("/", (req, res) => {
+  res.json({ message: "Sever is Running" });
+});
+
+startApolloServer();
+
+app.listen(port, () => {
+  console.log(`Server Listening on http://localhost:${port}`);
+});
